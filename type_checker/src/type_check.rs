9e2e2d1ca905a6054fe2d844507a9cb74aaa7771
evomassiny <hanzohatoryv@gmail.com>
2024-11-23 12:40:15 +0100

    revamp constraint store squeleton

--- type_checker/src/type_check.rs
+++ type_checker/src/type_check.rs
@@ -1,6 +1,9 @@
 use super::TypeTable;
-use parser::{Expr, ExprKind, Stmt, StmtKind};
+use parser::{Expr, ExprKind, Stmt, StmtKind, NodeId, LiteralKind};
 use resolver::{Ast, Sym, SymbolId};
+
+use std::collections::BTreeMap;
+
 
 #[derive(Debug)]
 pub enum TypeError {}
@@ -21,8 +24,10 @@ pub enum MathOp {
 
 pub enum Constraint {
     CanMathWith(MathOp, SymbolId),
-    IsNumber,
-    IsString,
+    IsNum,
+    IsStr,
+    IsBool,
+    IsNil,
     // equivalent to
     EqTo(SymbolId),
     ReturnTypeOf(SymbolId),
@@ -30,21 +35,51 @@ pub enum Constraint {
     HasAttr(String),
 }
 
-pub struct ConstraintSet(Vec<Constraint>);
-impl ConstraintSet {
-    pub fn new() -> Self {
-        Self(Vec::new())
-    }
+struct ConstraintStore {
+    // node ids are sorted, and contiguous node ids are often 
+    // read together so i expect that a btreemap is a nice fit for this
+    // read/write pattern.
+    // If performance somehow becomes an issue, we could use a flat array.
+    constraints: BTreeMap<NodeId, Vec<Constraint>>,
 }
+impl ConstraintStore {
+
+    pub fn new() -> Self {
+        Self{ constraints: BTreeMap::new() }
+    }
+
+    pub fn get(&self, node_id: NodeId) -> Option<&[Constraint]> {
+        None
+    }
+
+    pub fn add(&mut self, node_id: NodeId, constraint: Constraint) {
+        let mut set = self.constraints.entry(node_id).or_insert_with(Vec::new);
+        set.push(constraint)
+    }
+
+}
+
 
 fn collect_expression_constraints<'set>(
     expr: &Expr<Sym>,
-    set: &'set mut Vec<ConstraintSet>,
+    store: &'set mut ConstraintStore,
 ) -> Result<(), TypeError> {
 
+    // TODO:
+    // fill this ! 
+    //
     use ExprKind::*;
     match &expr.kind {
-        Literal(literal_kind) => todo!(),
+        Literal(literal_kind) => {
+            use LiteralKind::*;
+            let constraint = match &literal_kind {
+                Num(..) => Constraint::IsNum,
+                Str(..) => Constraint::IsStr,
+                Bool(..) => Constraint::IsBool,
+                Nil => Constraint::IsNil,
+            };
+            store.add(&expr.id, constraint);
+        },
         Unary(kind, inner_expr) => todo!(),
         Binary(left_expr, kind, right_expr) => todo!(),
         Logical(left_expr, kind, right_expr) => todo!(),
@@ -62,7 +97,7 @@ fn collect_expression_constraints<'set>(
 
 fn collect_constraints_in_stmt<'set>(
     stmt: &Stmt<Sym>,
-    set: &'set mut Vec<ConstraintSet>,
+    set: &'set mut ConstraintStore,
 ) -> Result<(), TypeError> {
 
     use StmtKind::*;
@@ -81,19 +116,16 @@ fn collect_constraints_in_stmt<'set>(
     Ok(())
 }
 
-fn collect_constraints(ast: &Ast) -> Result<Vec<ConstraintSet>, TypeError> {
-    // build a table a list of constraints, per variable symbol
-    let mut contraints_by_symbol: Vec<ConstraintSet> = Vec::with_capacity(ast.symbols.len());
-    for _ in 0..ast.symbols.len() {
-        contraints_by_symbol.push(ConstraintSet::new());
-    }
+fn collect_constraints(ast: &Ast) -> Result<ConstraintStore, TypeError> {
+
+    let mut contraints_by_ast_node: ConstraintStore = ConstraintStore::new();
 
     // traverse the ast and append constraints to each type
     for stmt in &ast.roots {
-        collect_constraints_in_stmt(stmt, &mut contraints_by_symbol)?;
+        collect_constraints_in_stmt(stmt, &mut contraints_by_ast_node)?;
     }
 
-    Ok(contraints_by_symbol)
+    Ok(contraints_by_ast_node)
 }
 
 pub fn type_check(untyped_ast: Ast) -> Result<TypedAst, TypeError> {
