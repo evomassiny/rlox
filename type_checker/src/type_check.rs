9e4aabf908f562f3bf446387638256a706452458
evomassiny <hanzohatoryv@gmail.com>
2025-06-28 13:13:32 +0200

    Continue adding type constraints

--- type_checker/src/type_check.rs
+++ type_checker/src/type_check.rs
@@ -14,13 +14,13 @@ pub enum TypeError {
 #[derive(Debug)]
 pub struct TypedAst {}
 
-/// Constraints are properties of
+/// Facts are properties of
 /// expression nodes that must be kept when
 /// picking the correct type for an expression.
 ///
 /// They are tied to AST nodes, identified by their `NodeId`.
 #[derive(Debug)]
-pub enum Constraint {
+pub enum Fact {
     IsNumOrStr(NodeId),
     IsNum(NodeId),
     IsStr(NodeId),
@@ -39,32 +39,36 @@ pub enum Constraint {
     // `a = 1` means `a` has the same type as `1`
     SymbolCanHoldTypeOf(SymbolId, NodeId),
     HasTypeOfSymbol(NodeId, SymbolId),
+    // Expression (0) as the type of the attribute (2)
+    // from the expression (1)
+    HasTypeOfObjAttr(NodeId, NodeId, String),
     IsClass(NodeId, SymbolId),
-    ClassHasAttr(SymbolId, String),
-    HasTypeOfAttr(NodeId, SymbolId, String),
+    IsObjectWithAttr(NodeId,  String),
+    ExprHasAttr(NodeId, String),
+    HasTypeOfClassAttr(NodeId, SymbolId, String),
 }
 
-struct ConstraintStore {
-    pub(crate) constraints: Vec<Constraint>,
+struct TypeSolver {
+    pub(crate) facts: Vec<Fact>,
     // those are traversal states
     // current class, current super class
     in_class_declaration: Option<(SymbolId, Option<SymbolId>)>,
 }
-impl ConstraintStore {
+impl TypeSolver {
     pub fn new() -> Self {
         Self {
-            constraints: Vec::new(),
+            facts: Vec::new(),
             in_class_declaration: None,
         }
     }
 
-    pub fn get(&self, node_id: NodeId) -> Option<&[Constraint]> {
+    pub fn get(&self, node_id: NodeId) -> Option<&[Fact]> {
         todo!()
     }
 
-    /// add a constraint to the store
-    pub fn add(&mut self, constraint: Constraint) {
-        self.constraints.push(constraint)
+    /// add a constraint to the solver
+    pub fn add_fact(&mut self, constraint: Fact) {
+        self.facts.push(constraint)
     }
 
     /// Set the class id we are currently traversing
@@ -93,55 +97,53 @@ impl ConstraintStore {
             _ => None,
         }
     }
-
-
 }
 
 /// given a binary expression, 
-/// collect the type constraints derived from
+/// collect the type facts derived from
 /// their usage.
-fn collect_binary_expression_constraints(
+fn collect_binary_expression_facts(
     binary_id: NodeId,
     left: &Expr<SymbolId>,
     kind: &BinaryExprKind,
     right: &Expr<SymbolId>,
-    store: &mut ConstraintStore,
+    solver: &mut TypeSolver,
 ) -> Result<(), TypeError> {
     use BinaryExprKind::*;
-    use Constraint::*;
+    use Fact::*;
 
-    // recursively collect constraints
-    let _ = collect_expression_constraints(left, store)?;
-    let _ = collect_expression_constraints(right, store)?;
+    // recursively collect facts
+    let _ = collect_expression_facts(left, solver)?;
+    let _ = collect_expression_facts(right, solver)?;
 
     match kind {
         // both operands can only be either strings or numbers
         // _and_ are equivalent
         Add => {
-            store.add(Same(left.id, right.id));
-            store.add(IsNumOrStr(left.id));
-            store.add(Same(binary_id, left.id));
+            solver.add_fact(Same(left.id, right.id));
+            solver.add_fact(IsNumOrStr(left.id));
+            solver.add_fact(Same(binary_id, left.id));
         }
         // both operands are numbers
         LessEqual | Less | GreaterEqual | Greater | Sub | Div | Mul => {
-            store.add(IsNum(left.id));
-            store.add(IsNum(right.id));
-            store.add(IsBool(binary_id));
+            solver.add_fact(IsNum(left.id));
+            solver.add_fact(IsNum(right.id));
+            solver.add_fact(IsBool(binary_id));
         }
         // both operands are equivalents
         NotEqual | Equal => {
-            store.add(Same(left.id, right.id));
-            store.add(IsBool(binary_id));
+            solver.add_fact(Same(left.id, right.id));
+            solver.add_fact(IsBool(binary_id));
         }
     }
     Ok(())
 }
 
-fn collect_expression_constraints<'set>(
+fn collect_expression_facts<'set>(
     expr: &Expr<SymbolId>,
-    store: &'set mut ConstraintStore,
+    solver: &'set mut TypeSolver,
 ) -> Result<(), TypeError> {
-    use Constraint::*;
+    use Fact::*;
     use ExprKind::*;
     match &expr.kind {
         // for str, number and bools,
@@ -149,9 +151,9 @@ fn collect_expression_constraints<'set>(
         // for Nil, we can't decide anything.
         Literal(literal_kind) => {
             match &literal_kind {
-                LiteralKind::Num(..) => store.add(IsNum(expr.id)),
-                LiteralKind::Str(..) => store.add(IsStr(expr.id)),
-                LiteralKind::Bool(..) => store.add(IsBool(expr.id)),
+                LiteralKind::Num(..) => solver.add_fact(IsNum(expr.id)),
+                LiteralKind::Str(..) => solver.add_fact(IsStr(expr.id)),
+                LiteralKind::Bool(..) => solver.add_fact(IsBool(expr.id)),
                 LiteralKind::Nil => {}
             };
         }
@@ -164,108 +166,114 @@ fn collect_expression_constraints<'set>(
                     // the `!exp` is a boolean
                     // anything can be casted as a boolean
                     // so it doesn't give us any more information
-                    store.add(IsBool(expr.id));
+                    solver.add_fact(IsBool(expr.id));
                 }
                 UnaryExprKind::Minus => {
                     // both the current expression and the innner one are numbers
-                    store.add(IsNum(expr.id));
-                    store.add(IsNum(inner_expr.id));
+                    solver.add_fact(IsNum(expr.id));
+                    solver.add_fact(IsNum(inner_expr.id));
                 }
             };
-            let _ = collect_expression_constraints(inner_expr, store)?;
+            let _ = collect_expression_facts(inner_expr, solver)?;
         }
-        Binary(left, kind, right) => collect_binary_expression_constraints(
-            expr.id, left, kind, right, store,
+        Binary(left, kind, right) => collect_binary_expression_facts(
+            expr.id, left, kind, right, solver,
         )?,
         Logical(left, _kind, right) => {
             // the type of the binary expression itself depends of the evaluation,
-            store.add(IsEither(expr.id, left.id, right.id));
-            let _ = collect_expression_constraints(left, store)?;
-            let _ = collect_expression_constraints(right, store)?;
+            solver.add_fact(IsEither(expr.id, left.id, right.id));
+            let _ = collect_expression_facts(left, solver)?;
+            let _ = collect_expression_facts(right, solver)?;
         }
         Grouping(inner_expr) => {
             // the inner node has the exact same type as its child
-            store.add(Same(expr.id, inner_expr.id));
-            let _ = collect_expression_constraints(inner_expr, store)?;
+            solver.add_fact(Same(expr.id, inner_expr.id));
+            let _ = collect_expression_facts(inner_expr, solver)?;
         }
         Call(callee_expr, args) => {
             // We known that "callee_expr" is a function.
             // which should be callable with those args
             let arg_ids = args.iter().map(|arg| arg.id).collect();
-            store.add(IsCallableWith(callee_expr.id, arg_ids));
+            solver.add_fact(IsCallableWith(callee_expr.id, arg_ids));
             // we also know that the call expression
             // has the type of the return value of the expression.
-            store.add(ReturnTypeOf(callee_expr.id));
+            solver.add_fact(ReturnTypeOf(callee_expr.id));
 
-            let _ = collect_expression_constraints(callee_expr, store)?;
+            let _ = collect_expression_facts(callee_expr, solver)?;
             for arg_expr in args {
-                let _ = collect_expression_constraints(arg_expr, store)?;
+                let _ = collect_expression_facts(arg_expr, solver)?;
             }
         }
         Assign(bind_name, r_value_expr) => {
             // The assign expression itself is nil
-            store.add(IsNil(expr.id));
+            solver.add_fact(IsNil(expr.id));
             // the object can store the type of the `r_value_expr`
-            store.add(SymbolCanHoldTypeOf(*bind_name, r_value_expr.id));
-            let _ = collect_expression_constraints(r_value_expr, store)?;
+            solver.add_fact(SymbolCanHoldTypeOf(*bind_name, r_value_expr.id));
+            let _ = collect_expression_facts(r_value_expr, solver)?;
         }
         Variable(bind_name) => {
             // The bind expression itself is nil
-            store.add(IsNil(expr.id));
+            solver.add_fact(IsNil(expr.id));
             // variable has the type of the r_value
-            store.add(HasTypeOfSymbol(expr.id, *bind_name));
+            solver.add_fact(HasTypeOfSymbol(expr.id, *bind_name));
         }
         Get(object_expr, attr_name) => {
-            todo!()
+            solver.add_fact(IsObjectWithAttr(object_expr.id, attr_name.clone()));
+            let _ = collect_expression_facts(object_expr, solver)?;
+            solver.add_fact(HasTypeOfObjAttr(expr.id, object_expr.id, attr_name.clone()));
         }
-        Set(object_expr, attr_name, r_value_expr) => todo!(),
+        Set(object_expr, attr_name, r_value_expr) => {
+            solver.add_fact(IsObjectWithAttr(object_expr.id, attr_name.clone()));
+            let _ = collect_expression_facts(object_expr, solver)?;
+            let _ = collect_expression_facts(r_value_expr, solver)?;
+        },
         Super(attr_name) => {
-            let super_class = store.get_current_super_class()
+            let super_class = solver.get_current_super_class()
                 .ok_or(TypeError::NoSuperClass)?;
-            store.add(IsClass(expr.id, super_class));
-            store.add(HasTypeOfAttr(expr.id, super_class, attr_name.clone()));
+            solver.add_fact(IsClass(expr.id, super_class));
+            solver.add_fact(HasTypeOfClassAttr(expr.id, super_class, attr_name.clone()));
         }
         This => {
-            let this_class = store.get_current_class()
+            let this_class = solver.get_current_class()
                 .ok_or(TypeError::NotInClass)?;
-            store.add(IsClass(expr.id, this_class));
+            solver.add_fact(IsClass(expr.id, this_class));
         }
     };
     Ok(())
 }
 
-fn collect_constraints_in_stmt<'set>(
+fn collect_facts_in_stmt<'set>(
     stmt: &Stmt<SymbolId>,
-    set: &'set mut ConstraintStore,
+    set: &'set mut TypeSolver,
 ) -> Result<(), TypeError> {
-    use Constraint::*;
+    use Fact::*;
     use StmtKind::*;
 
     match &stmt.kind {
         Block(stmts) => {
             for stmt in stmts {
-                let _ = collect_constraints_in_stmt(stmt, set)?;
+                let _ = collect_facts_in_stmt(stmt, set)?;
             }
         }
         Class(name, maybe_super_name, methods) => {
             set.set_current_class(name, maybe_super_name);
             for method in methods {
-                let _ = collect_constraints_in_stmt(stmt, set)?;
+                let _ = collect_facts_in_stmt(stmt, set)?;
             }
             set.clear_current_class();
         }
         If(condition, then, maybe_else) => todo!("If stmt"),
         Function(name, args, body) => todo!("Function"),
         Expr(expr) => {
-            let _ = collect_expression_constraints(expr, set)?;
+            let _ = collect_expression_facts(expr, set)?;
         }
         Print(expr) => {
-            let _ = collect_expression_constraints(expr, set)?;
+            let _ = collect_expression_facts(expr, set)?;
         }
         Return(maybe_expr) => todo!("Return"),
         Var(name, intializer) => {
-            let _ = collect_expression_constraints(intializer, set)?;
-            set.add(SymbolCanHoldTypeOf(*name, intializer.id))
+            let _ = collect_expression_facts(intializer, set)?;
+            set.add_fact(SymbolCanHoldTypeOf(*name, intializer.id))
         }
         While(condition, body) => todo!("While loops"),
         For(maybe_initializer, maybe_condition, maybe_increment, body) => {
@@ -275,12 +283,12 @@ fn collect_constraints_in_stmt<'set>(
     Ok(())
 }
 
-fn collect_constraints(ast: &Ast) -> Result<ConstraintStore, TypeError> {
-    let mut contraints_by_ast_node: ConstraintStore = ConstraintStore::new();
+fn collect_facts(ast: &Ast) -> Result<TypeSolver, TypeError> {
+    let mut contraints_by_ast_node: TypeSolver = TypeSolver::new();
 
-    // traverse the ast and append constraints to each type
+    // traverse the ast and append facts to each type
     for stmt in &ast.roots {
-        collect_constraints_in_stmt(stmt, &mut contraints_by_ast_node)?;
+        collect_facts_in_stmt(stmt, &mut contraints_by_ast_node)?;
     }
 
     Ok(contraints_by_ast_node)
@@ -288,12 +296,12 @@ fn collect_constraints(ast: &Ast) -> Result<ConstraintStore, TypeError> {
 
 pub fn type_check(untyped_ast: Ast) -> Result<TypedAst, TypeError> {
     // done in two part:
-    // * first we collect a bunch of constraints for each type,
+    // * first we collect a bunch of facts for each type,
     //   for instance `a + c` means `a` and `c` are of the same type,
     //   and this type implements `add`
-    // * then we actually solve thoses constraints
-    let constraints = collect_constraints(&untyped_ast)?;
-    for constraint in &constraints.constraints {
+    // * then we actually solve thoses facts
+    let facts = collect_facts(&untyped_ast)?;
+    for constraint in &facts.facts {
         println!("{constraint:?}");
     }
     let types = TypeTable::new();
